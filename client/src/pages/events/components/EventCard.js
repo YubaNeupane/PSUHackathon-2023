6046da4a41b7cc879d0d2d26799e4ee0f5b113c2
Mateen0018 <98054516+Mateen0018@users.noreply.github.com>
2023-04-02 06:34:22 -0400

    made changes to the event card page

--- client/src/pages/events/components/EventCard.js
+++ client/src/pages/events/components/EventCard.js
@@ -8,32 +8,67 @@ const EventCard = () => {
 
   const [toggleModel, showModel] = useState(false);
 
+  console.log(events);
+
   return (
     <div className="content-container w-full  left-[22.5rem] top-0">
       <TopNavigation />
 
       <div className="event-container">
         {events.map((item) => (
+          // <div
+          //   className="event-card h-[71%]"
+          //   onClick={() => console.log(item._id)}
+          // >
+          //   <img
+          //     className="w-full object-cover "
+          //     width={100}
+          //     height={100}
+          //     src="https://1000logos.net/wp-content/uploads/2017/11/penn-state-logo.png"
+          //     alt="psu logo"
+          //   ></img>
+          //   <div className=" object-cover h-fit w-fit bg-red-500 pl-4 pr-4 justify-start">
+          //     <div className="px-0">
+          //       <div className="event-card-title">{item.name}</div>
+          //       <p className=" event-card-text">
+          //         {item.description}
+          //       </p>
+          //       <p className="event-card-text">Date: {item.date}</p>
+          //       <p className="event-card-text">Time: {item.date}</p>
+
+          //       <p className="event-card-text pb-0">
+          //         {item.category?.map((cat) => (
+          //           <span class="event-card-tag">{cat}</span>
+          //         ))}
+          //       </p>
+          //     </div>
+          //   </div>
+          // </div>
           <div
-            className="event-card h-fit"
+            className="event-card h-[71%] flex flex-col"
             onClick={() => console.log(item._id)}
           >
             <img
-              className="w-full"
+              className="w-full object-cover"
+              width={100}
+              height={100}
               src="https://1000logos.net/wp-content/uploads/2017/11/penn-state-logo.png"
               alt="psu logo"
             ></img>
-            <div className="px-6 py-4">
-              <div className="event-card-title">{item.name}</div>
-              <p className="event-card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              <span class="event-card-tag">academic</span>
-              <span className="event-card-tag">food</span>
+            <div className=" object-cover  pl-4 pr-4 justify-start flex-grow">
+              <div className="px-0">
+                <div className="event-card-title">{item.name}</div>
+                <p className="event-card-text overflow-hidden whitespace max-w-[600px]">
+                  {item.description}
+                </p>
+                <p className="event-card-text">Date: {item.date}</p>
+                <p className="event-card-text">Time: {item.date}</p>
+                <p className="event-card-text pb-0">
+                  {item.category?.map((cat) => (
+                    <span class="event-card-tag">{cat}</span>
+                  ))}
+                </p>
+              </div>
             </div>
           </div>
         ))}
