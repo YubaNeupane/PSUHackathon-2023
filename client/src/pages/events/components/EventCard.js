36aa72359f25b8cd6a4faa1f55c95ee1fc4b97d0
NotSaifA <69015839+NotSaifA@users.noreply.github.com>
2023-04-02 02:24:27 -0400

    Added create event popup

--- client/src/pages/events/components/EventCard.js
+++ client/src/pages/events/components/EventCard.js
@@ -1,9 +1,13 @@
 import TopNavigation from "../../Dashboard/Components/EventNavigation";
+import Popup from "../components/Popup";
+import { useState } from "react";
 
 const EventCard = () => {
+  const [showModel, toggleModel] = useState(false);
   return (
     <div className="content-container w-full  left-[22.5rem] top-0">
       <TopNavigation />
+
       <div className="event-container">
         <div className="event-card">
           <img
@@ -20,7 +24,7 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
@@ -39,7 +43,7 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
@@ -58,7 +62,7 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
@@ -77,7 +81,7 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
@@ -96,7 +100,7 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
@@ -115,27 +119,16 @@ const EventCard = () => {
             </p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span class="event-card-tag">academic</span>
+            <span className="event-card-tag">academic</span>
             <span className="event-card-tag">food</span>
           </div>
         </div>
       </div>
       <button
-        class="p-0 w-12 h-12 bg-darkYellow text-darkGreen dark:text-white dark:bg-lightGreen 
-         hover:bg-darkGreen dark:hover:bg-darkYellow hover:text-white rounded-full active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none fixed bottom-5 right-5"
+        onClick={() => toggleModel(!showModel)}
+        className="fixed bottom-5 right-5"
       >
-        <svg
-          viewBox="0 0 20 20"
-          enable-background="new 0 0 20 20"
-          class="w-6 h-6 inline-block"
-        >
-          <path
-            fill="#FFFFFF"
-            d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
-                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
-                                    C15.952,9,16,9.447,16,10z"
-          />
-        </svg>
+        <Popup />
       </button>
     </div>
   );
