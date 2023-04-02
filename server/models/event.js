023bb8f914919e38d4db8cd617549c3bb77a1497
Kevin Muirhead <kxm5887@psu.edu>
2023-04-02 11:33:28 -0400

    Filtering logic

--- server/models/event.js
+++ server/models/event.js
@@ -4,14 +4,21 @@ import mongoose from "mongoose";
 //TODO: Make chnages to this schema
 const eventSchema = mongoose.Schema({
   name: { type: String, require: true },
-  creatorId: { type: String, require: true },
+  creatorName: { type: String, require: true },
   creationData: { type: String, require: true, default: Date.now },
-  time: { type: Date, require: true },
-  location: { type: String },
-  description: { type: String, required: true },
-  poster: { type: String },
-  category: { type: [String] },
-  type: { type: [String] },
+  date: { type: Date, require: true },
+  eventType: {type: Array, require: true},
+  category: {type: Array, require: true},
+  location: { type: String, require: true},
+  description: {type: String, required: true},
+
+  servers: [{ name: { type: String }, id: { type: mongoose.Schema.ObjectId } }],
+  directMessages: [
+    { name: { type: String }, id: { type: mongoose.Schema.ObjectId } },
+  ],
+  admin: { type: mongoose.Schema.ObjectId, default: null },
 });
 
+
 export default mongoose.model("Event", eventSchema);
+
